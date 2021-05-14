c9fc1091365a364cb1a29e52cc8aea3838b25f96
Isabel Fernandez <izyf98@gmail.com>
2021-05-14 12:02:54 -0400

    added code on fashion data for comparison

--- Notebooks/Comparing_Data.py
+++ Notebooks/Comparing_Data.py
@@ -36,6 +36,7 @@ import sklearn
 from sklearn.datasets import load_digits
 from sklearn.metrics import pairwise_distances
 import holoviews as hv
+from holoviews.operation.datashader import rasterize
 import panel as pn
 from holoviews import dim, opts
 hv.extension('bokeh')
@@ -50,28 +51,47 @@ PRJDIR = '/data/SFIMJGC/PRJ_Manifold_Learning' # Project directory path
 # * Number of features: 64 (8x8)
 # * Number of groups: 10 (0-9)
 # * Pixle value range: 0-16
-# * Number of samples in groups: (0, 178), (1, 182), (2, 177), (3, 183), (4, 181), (5, 182), (6, 181), (7, 179), (8, 174), (9, 180)
+#  
+# | Digit | Number of Samples |
+# | :---: | :---: |
+# | 0 | 178 |
+# | 1 | 182 |
+# | 2 | 177 |
+# | 3 | 183 |
+# | 4 | 181 |
+# | 5 | 182 |
+# | 6 | 181 |
+# | 7 | 179 |
+# | 8 | 174 |
+# | 9 | 180 |
 
+# +
 # Load digits data
 # ----------------
-dig_data_df, dig_num_df = load_digits(return_X_y=True,as_frame=True)
-print('++ INFO: Digits data frame dimension ',dig_data_df.shape)
+dig_img_df, dig_lab_df = load_digits(return_X_y=True,as_frame=True)
+
+dig_num_samp = dig_img_df.shape[0]
+
+print('++ INFO: Digits data frame dimension ',dig_img_df.shape)
+# -
+
+dig_lab_df.value_counts(ascending=True)
 
 # Compute correlation and distance matrix
 # ---------------------------------------
-dig_corr = np.corrcoef(dig_data_df) # Correlation matrix
-dig_dist = pairwise_distances(dig_data_df, metric='euclidean') # Distance matrix
+dig_corr = np.corrcoef(dig_img_df) # Correlation matrix
+dig_dist = pairwise_distances(dig_img_df, metric='euclidean') # Distance matrix
 
 # Compute distribution of correlation and distance matrix
 # -------------------------------------------------------
-triangle = np.mask_indices(dig_data_df.shape[0], np.triu, k=1) # Top triangle mask for matricies
+triangle = np.mask_indices(dig_num_samp, np.triu, k=1) # Top triangle mask for matricies
 dig_corr_freq, dig_corr_edges = np.histogram(np.array(dig_corr)[triangle], 50) # Compute histogram of top triangle of correlation matrix (50 bars)
 dig_dist_freq, dig_dist_edges = np.histogram(np.array(dig_dist)[triangle], 50) # Compute histogram of top triangle of distance matrix (50 bars)
 
 # Create matrix and histogram plots
 # ---------------------------------
-dig_corr_img = hv.Image(dig_corr, bounds=(-0.5, -0.5, 1797.5, 1797.5)).opts(colorbar=True, height=300, width=400, title='Correlation Matrix')
-dig_dist_img = hv.Image(dig_dist, bounds=(-0.5, -0.5, 1797.5, 1797.5)).opts(colorbar=True, height=300, width=400, title='Distance Matrix')
+dig_corr_img = hv.Image(dig_corr, bounds=(-0.5, -0.5, dig_num_samp-1.5, dig_num_samp-1.5)).opts(colorbar=True, height=300, width=400, title='Correlation Matrix')
+dig_dist_img = hv.Image(dig_dist, bounds=(-0.5, -0.5, dig_num_samp-1.5, dig_num_samp-1.5)).opts(colorbar=True, height=300, width=400, title='Distance Matrix')
 dig_corr_his = hv.Histogram((dig_corr_edges, dig_corr_freq)).opts(xlabel='Correlation', height=300, width=400, title='Correlation Histogram')
 dig_dist_his = hv.Histogram((dig_dist_edges, dig_dist_freq)).opts(xlabel='Distance', height=300, width=400, title='Distance Histogram')
 
@@ -83,35 +103,59 @@ dig_dist_his = hv.Histogram((dig_dist_edges, dig_dist_freq)).opts(xlabel='Distan
 # ## Fashion Data
 #
 # The MNIST fashion data is loaded from https://github.com/zalandoresearch/fashion-mnist. The data set consists of a training set of 60,000 images and a test set of 10,000 images. Each image is a 28x28 grayscale image, associated with a label from 10 classes. 
-# * Number of samples: 70,000 (60,000 train + 10,000 test)
+# * Number of samples: 10,000 (test only)
 # * Number of features: 784 (28x28)
 # * Number of groups: 10
-# * Pixle value range: 
-# * Number of samples in groups: 
+# * Pixle value range: ??
 #
-# | Label | Description |
-# | ----- | ----------- |
-# | 0 | T-shirt/top |
-# | 1 | Trouser |
-# | 2 | Pullover |
-# | 3 | Dress |
-# | 4 | Coat |
-# | 5 | Sandal |
-# | 6 | Shirt |
-# | 7 | Sneaker |
-# | 8 | Bag |
-# | 9 | Ankle boot |
+# | Label | Description | Number of Samples |
+# | :---: | :---: | :---: |
+# | 0 | T-shirt/top | 1000 |
+# | 1 | Trouser | 1000 |
+# | 2 | Pullover | 1000 |
+# | 3 | Dress | 1000 |
+# | 4 | Coat | 1000 |
+# | 5 | Sandal | 1000 |
+# | 6 | Shirt | 1000 |
+# | 7 | Sneaker | 1000 |
+# | 8 | Bag | 1000 |
+# | 9 | Ankle boot | 1000 |
 
 # +
-# Load fashion data
-# -----------------
-fashion_path = os.path.join(PRJDIR,'Data','Fashion_Data')
+# Load fashion data (test only)
+# -----------------------------
+fashion_path = os.path.join(PRJDIR,'Data','Fashion_Data') # Path to fashion data set
 
-fash_test_img  = np.load(fashion_path+'/test_images.npy')
-fash_test_lab  = np.load(fashion_path+'/test_labels.npy')
-fash_train_img = np.load(fashion_path+'/train_images.npy')
-fash_train_lab = np.load(fashion_path+'/train_labels.npy')
+fash_test_img  = np.load(fashion_path+'/test_images.npy') # Load test images
+fash_test_lab  = np.load(fashion_path+'/test_labels.npy') # Load test labels
 
-fash_img = np.concatenate((fash_test_img, fash_train_img), axis=0)
+fash_num_samp = fash_test_img.shape[0]
 
-fash_lab_df = pd.DataFrame(np.concatenate((fash_test_lab, fash_train_lab), axis=0))
+fash_img_df = pd.DataFrame(fash_test_img.reshape((fash_num_samp, 784))) # Flatten image matricies and convert images array to pandas df
+fash_lab_df = pd.DataFrame(fash_test_lab) # Convert lables array to pandas df
+
+print('++ INFO: Digits data frame dimension ',fash_img_df.shape)
+# -
+
+# Compute correlation and distance matrix
+# ---------------------------------------
+fash_corr = np.corrcoef(fash_img_df) # Correlation matrix
+fash_dist = pairwise_distances(fash_img_df, metric='euclidean') # Distance matrix
+
+# Compute distribution of correlation and distance matrix
+# -------------------------------------------------------
+triangle = np.mask_indices(fash_num_samp, np.triu, k=1) # Top triangle mask for matricies
+fash_corr_freq, fash_corr_edges = np.histogram(np.array(fash_corr)[triangle], 50) # Compute histogram of top triangle of correlation matrix (50 bars)
+fash_dist_freq, fash_dist_edges = np.histogram(np.array(fash_dist)[triangle], 50) # Compute histogram of top triangle of distance matrix (50 bars)
+
+# Create matrix and histogram plots
+# ---------------------------------
+# raterize() fucntion used for big data set
+fash_corr_img = rasterize(hv.Image(fash_corr, bounds=(-0.5, -0.5, fash_num_samp-1.5, fash_num_samp-1.5)).opts(colorbar=True, height=300, width=400, title='Correlation Matrix'))
+fash_dist_img = rasterize(hv.Image(fash_dist, bounds=(-0.5, -0.5, fash_num_samp-1.5, fash_num_samp-1.5)).opts(colorbar=True, height=300, width=400, title='Distance Matrix'))
+fash_corr_his = rasterize(hv.Histogram((fash_corr_edges, fash_corr_freq)).opts(xlabel='Correlation', height=300, width=400, title='Correlation Histogram'))
+fash_dist_his = rasterize(hv.Histogram((fash_dist_edges, fash_dist_freq)).opts(xlabel='Distance', height=300, width=400, title='Distance Histogram'))
+
+# Display all digit data plots
+# ----------------------------
+(fash_corr_img+fash_corr_his+fash_dist_img+fash_dist_his).opts(opts.Layout(shared_axes=False)).cols(2)
